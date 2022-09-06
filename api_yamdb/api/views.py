63ec01dd68e1c4c661bb1be1663ccf2b6307438d
Funnysuslik <stepanov.step@gmail.com>
2022-09-06 17:52:41 +0300

    Add Viewsets for Reviews and Comments. Add permissions.py and serializers.py in api app

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import mixins
 
-# Create your views here.
+from titles.models import Comment, Review
+from .serializers import CommentsSerializer, ReviewsSerializer
+from .permissions import IsAuthorOrModeratorOrReadOnly
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    """Viewset for reviews model."""
+    queryset = Review.objects.all()
+    serializer_class = ReviewsSerializer
+    permission_classes = [IsAuthorOrModeratorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    """Viewset for reviews model."""
+    queryset = Comment.objects.all()
+    serializer_class = CommentsSerializer
+    permission_classes = [IsAuthorOrModeratorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
