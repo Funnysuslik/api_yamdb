38170a6f2ed8c991f1f28d78b43cf4b0b523c642
Guzovvv <guzovvv@yandex.ru>
2022-09-07 00:28:09 +0300

    Update models.py

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator  # , MinValueValidator
 from django.db import models
+from django.utils import timezone
 
 
 class CustomUser(AbstractUser):
@@ -8,6 +10,75 @@ class CustomUser(AbstractUser):
         'some description',
         blank=True,
     )
+
+
+class NameSlug(models.Model):
+    name = models.CharField(
+        verbose_name='Наименование',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        max_length=50,
+        unique=True
+    )
+
+
+class Category(NameSlug):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(NameSlug):
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+def year():
+    return timezone.now().year
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        validators=[
+            MaxValueValidator(
+                year, 'Значение не должно быть больше текущей даты!'
+            )
+        ]
+    )
+    rating = models.IntegerField(
+        verbose_name='Рейтинг на основе отзывов',
+        null=True,
+        default=None
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        related_name='titles',
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
 
 class Review(models.Model):
@@ -62,3 +133,69 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text[:10]
+
+
+
+
+"""
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='ID отзыва',
+    )
+    text = models.TextField(
+        'Текст отзыва',
+        blank=False,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Username пользователя'
+    )
+    score = models.PositiveSmallIntegerField(
+        'Оценка',
+        validators=[
+            MinValueValidator(1, 'Минимальная оценка - 1'),
+            MaxValueValidator(10, 'Максимальная оценка - 10')
+        ]
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='ID комментария'
+    )
+    text = models.TextField(
+        'Текст комментария',
+        blank=False,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Username автора комметария'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации комментария',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+"""
