c03fdcf8472a28144038b2fd1b64a667dab9fbf3
Dmitrii Barilkin <dimabaril@gmail.com>
2022-09-09 16:55:27 +0400

    Вроде всё поправил

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,41 +1,31 @@
-"""Views."""
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import mixins
 
-
-from titles.models import Comment, Review, Review, Title
+from titles.models import Comment, Review
 from .serializers import CommentsSerializer, ReviewsSerializer
-#from .permissions import IsAuthorOrModeratorOrReadOnly
+from .permissions import IsAuthorOrModeratorOrReadOnly
 
 
 class ReviewsViewSet(viewsets.ModelViewSet):
     """Viewset for reviews model."""
 
+    queryset = Review.objects.all()
     serializer_class = ReviewsSerializer
-    #permission_classes = [IsAuthorOrModeratorOrReadOnly]
-
-    def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-
-        return title.reviews.all()
+    permission_classes = [IsAuthorOrModeratorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
-    """Viewset for comments model."""
+    """Viewset for reviews model."""
 
+    queryset = Comment.objects.all()
     serializer_class = CommentsSerializer
-    #permission_classes = [IsAuthorOrModeratorOrReadOnly]
-
-    def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
-
-        return review.comments.all()
+    permission_classes = [IsAuthorOrModeratorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
