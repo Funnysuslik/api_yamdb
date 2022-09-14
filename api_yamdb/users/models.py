2abbeac26c104b8b7602abf1d5e99cc33acf0244
Dmitrii Barilkin <dimabaril@gmail.com>
2022-09-14 22:23:22 +0400

    not all fixed

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -10,40 +10,34 @@ class MyUserManager(UserManager):
     Зарезервированное имя использовать нельзя."""
 
     def create_user(self, username, email, password, **extra_fields):
-
         if not email:
-
             raise ValueError('Поле email обязательное')
-
         if username == RESERVED_NAME:
-
             raise ValueError(MESSAGE_FOR_RESERVED_NAME)
-
         return super().create_user(
             username, email, password, **extra_fields)
 
     def create_superuser(
             self, username, email, password, role='admin', **extra_fields):
-
         return super().create_superuser(
             username, email, password, role='admin', **extra_fields)
 
 
 class User(AbstractUser):
     """Our custom user model."""
-
+    USER = 'user'
+    MODER = 'moderator'
+    ADMIN = 'admin'
     ROLES_CHOICES = (
-        ('user', 'user'),
-        ('moderator', 'moderator'),
-        ('admin', 'admin')
+        (USER, USER),
+        (MODER, MODER),
+        (ADMIN, ADMIN),
     )
     bio = models.TextField(blank=True)
     role = models.CharField(
         max_length=50, choices=ROLES_CHOICES, default='user')
     username = models.CharField(max_length=50, unique=True)
-
     objects = MyUserManager()
-
     REQUIRED_FIELDS = ('email', 'password')
 
     class Meta:
@@ -52,15 +46,12 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-
-        return self.role == self.ROLES_CHOICES[2][1]
+        return self.role == self.ADMIN
 
     @property
     def is_moderator(self):
-
-        return self.role == self.ROLES_CHOICES[1][1]
+        return self.role == self.MODER
 
     @property
     def is_user(self):
-
-        return self.role == self.ROLES_CHOICES[0][1]
+        return self.role == self.USER
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -15,29 +15,28 @@ from users.serializers import (ForAdminSerializer, ForUserSerializer,
 from .models import User
 
 
-def create_confirmation_code_and_send_email(username):
-    user = get_object_or_404(User, username=username)
-    confirmation_code = default_token_generator.make_token(user)
-    send_mail(
-        subject=f'{user.username} Confirmation code',
-        message=f'Your confirmation code {confirmation_code}',
-        from_email=None,
-        recipient_list=[user.email, ])
-
-
 class APISignUp(APIView):
     """User auth"""
 
     permission_classes = (AllowAny, )
+
+    @staticmethod
+    def create_confirmation_code_and_send_email(username):
+        user = get_object_or_404(User, username=username)
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            subject=f'{user.username} Confirmation code',
+            message=f'Your confirmation code {confirmation_code}',
+            from_email=None,
+            recipient_list=[user.email, ])
 
     def post(self, request):
         serializer = ForUserSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            create_confirmation_code_and_send_email(
+            self.create_confirmation_code_and_send_email(
                 serializer.data['username'])
-
             return Response(
                 {'email': serializer.data['email'],
                  'username': serializer.data['username']},
@@ -55,14 +54,11 @@ class APIToken(APIView):
         if serializer.is_valid(raise_exception=True):
             user = get_object_or_404(
                 User, username=serializer.data['username'])
-
             if default_token_generator.check_token(
                     user, serializer.data['confirmation_code']):
                 token = AccessToken.for_user(user)
-
                 return Response(
                     {'token': str(token)}, status=status.HTTP_200_OK)
-
             return Response({
                 'confirmation code': 'Некорректный код подтверждения!'},
                 status=status.HTTP_400_BAD_REQUEST)
@@ -74,25 +70,20 @@ class APIUser(APIView):
     def get(self, request, *args, **kwargs):
         user = get_object_or_404(User, username=request.user.username)
         serializer = ForUserSerializer(user, many=False)
-
         return Response(serializer.data)
 
     def patch(self, request, *args, **kwargs):
         user = get_object_or_404(User, username=request.user.username)
         serializer = ForUserSerializer(
             user, data=request.data, partial=True, many=False)
-
         if serializer.is_valid():
             serializer.save()
-
             return Response(serializer.data, status=status.HTTP_200_OK)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSetForAdmin(ModelViewSet):
     """Работа с пользователями для администратора"""
-
     permission_classes = (IsAdmin, )
     queryset = User.objects.all()
     serializer_class = ForAdminSerializer
