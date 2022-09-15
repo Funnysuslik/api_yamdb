d1b88091c1209e5ed642d2c86bee324a702c7173
Stepanov Aleksandr <54445230+Funnysuslik@users.noreply.github.com>
2022-09-15 10:55:26 +0300

    Update models.py
    Delete "from django.utils import timezone" (moved to helpers.py)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,5 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.utils import timezone
 
 from .helpers import year
 from users.models import User
